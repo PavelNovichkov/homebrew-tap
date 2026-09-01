36447df5fcc3ca1c5db2491077bba46afe3f9679
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2026-09-01 22:32:32 +0200

    git-credential-gopass: Bump version

--- Formula/git-credential-gopass.rb
+++ Formula/git-credential-gopass.rb
@@ -1,8 +1,8 @@
 class GitCredentialGopass < Formula
   desc "Git credential helper that stores credentials in gopass"
   homepage "https://github.com/gopasspw/git-credential-gopass"
-  url "https://github.com/gopasspw/git-credential-gopass/archive/refs/tags/v1.16.1.tar.gz"
-  sha256 "8382b98e56b4547232cc5ea8967eb2a7497a9821a50499d215dafc2f530ba32e"
+  url "https://github.com/gopasspw/git-credential-gopass/archive/refs/tags/v1.17.0.tar.gz"
+  sha256 "2efe7cc34ad18f4e12368d24528c5b589c9d0796a606ede8154ad6a412b05ad0"
   license "MIT"
   head "https://github.com/gopasspw/git-credential-gopass.git", branch: "master"
 
